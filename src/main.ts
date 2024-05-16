f67a479b7891128bc8c486afd07a7a1b549a0b26
Raheem Kawojue <kawojue08@gmail.com>
2024-05-16 01:30:39 +0100

    refactored

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,42 @@
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
-import * as passport from 'passport';
-import { AppModule } from './app.module';
-import * as session from 'express-session';
-import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import * as express from 'express'
+import * as passport from 'passport'
+import { AppModule } from './app.module'
+import * as session from 'express-session'
+import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const PORT: number = parseInt(process.env.PORT, 10) || 2005;
-  const app = await NestFactory.create(AppModule);
+  const PORT: number = parseInt(process.env.PORT, 10) || 2005
+  const app = await NestFactory.create(AppModule)
 
   app.enableCors({
     origin: [
+      'https://memegoat.io',
       'http://localhost:3000',
       `http://localhost:${PORT}`,
-      'https://memegoat.io',
-      'https://socialfi.memegoat.io',
-      'https://memegoat-socialfi-backend.onrender.com',
       'https://testing.memegoat.io',
-      'https://api-socialfi.memegoat.io/dashboard',
+      'https://socialfi.memegoat.io',
       'https://memegoat.onrender.com',
-      'https://memegoat-client.vercel.app',
+      'https://api-socialfi.memegoat.io',
+      'https://memegoat-socialfi-backend.onrender.com',
     ],
-    methods: 'GET,POST,OPTIONS',
     credentials: true,
-  });
+    optionsSuccessStatus: 200,
+    methods: 'GET,POST,OPTIONS',
+  })
 
-  app.use(express.json({ limit: 2 << 20 }));
-  app.use(cookieParser());
+  app.use(express.json({ limit: 2 << 20 }))
+  app.use(cookieParser())
   app.use(
     session({
       resave: false,
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET!,
     }),
-  );
-  app.use(passport.session());
-  app.use(passport.initialize());
+  )
+  app.use(passport.session())
+  app.use(passport.initialize())
 
   const swaggerOptions = new DocumentBuilder()
     .setTitle('Memegoat API')
@@ -44,16 +44,16 @@ async function bootstrap() {
     .addServer(`https://api-socialfi.memegoat.io`, 'Staging')
     .addServer(`http://localhost:${PORT}`, 'Local')
     .addBearerAuth()
-    .build();
+    .build()
 
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
-  SwaggerModule.setup('docs', app, swaggerDocument);
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions)
+  SwaggerModule.setup('docs', app, swaggerDocument)
 
   try {
-    await app.listen(PORT);
-    console.log(`http://localhost:${PORT}`);
+    await app.listen(PORT)
+    console.log(`http://localhost:${PORT}`)
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
 }
-bootstrap();
+bootstrap()
