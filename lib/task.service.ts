186b8e05afbb6c6f7d81a441f98384cb21f6a428
Raheem Kawojue <kawojue08@gmail.com>
2024-05-17 11:54:13 +0100

    perf: task

--- lib/task.service.ts
+++ lib/task.service.ts
@@ -27,9 +27,9 @@ export class TaskService {
 
             await Promise.all(users.map(async (user) => {
                 const { data: { data: tweets } } = await this.x.v2.userTimeline(user.profileId, {
-                    max_results: 25,
+                    max_results: 10,
                     expansions: 'referenced_tweets.id',
-                    'tweet.fields': 'author_id,public_metrics',
+                    'tweet.fields': 'author_id,public_metrics,created_at',
                 })
 
                 const tweetIds = tweets.map(tweet => tweet.id)
@@ -38,30 +38,32 @@ export class TaskService {
                 })
                 const existingTweetMap = new Map(existingTweets.map(tweet => [tweet.postId, tweet]))
 
-                const tweetPromises = tweets.map(async ({ id, public_metrics, text, referenced_tweets, author_id }) => {
-
+                const tweetPromises = tweets.map(async ({
+                    id, author_id, text, created_at,
+                    referenced_tweets, public_metrics,
+                }) => {
                     const tags = settings.tags.map((tag) => tag.toLowerCase().trim())
                     const containsTag = tags.some(tag => text.toLowerCase().trim().includes(tag))
 
                     if (containsTag) {
                         let referenced = false
-
                         if (author_id !== settings.profileId) {
                             if (referenced_tweets) {
                                 await Promise.all(referenced_tweets.map(async ({ id }) => {
                                     const { data } = await this.x.v2.singleTweet(id, {
                                         'tweet.fields': 'author_id',
                                     })
-                                    if (data.author_id === settings.profileId) {
-                                        referenced = true
+                                    if (data.author_id !== user.profileId) {
+                                        if (data.author_id === settings.profileId) {
+                                            referenced = true
+                                        }
                                     }
                                 }))
                             }
                         }
 
                         const existingTweet = existingTweetMap.get(id)
-
-                        if (existingTweet && public_metrics.impression_count > existingTweet.impression) return
+                        if (existingTweet && public_metrics.impression_count == existingTweet.impression) return
 
                         if (existingTweet) {
                             await this.prisma.tweet.update({
@@ -73,19 +75,20 @@ export class TaskService {
                                     quote: public_metrics.quote_count,
                                     retweet: public_metrics.retweet_count,
                                     impression: public_metrics.impression_count,
+                                    postedAt: created_at ? new Date(created_at) : undefined,
                                 },
                             })
                         } else {
                             await this.prisma.tweet.create({
                                 data: {
-                                    postId: id,
-                                    referenced,
+                                    postId: id, referenced,
                                     like: public_metrics.like_count,
                                     reply: public_metrics.reply_count,
                                     quote: public_metrics.quote_count,
                                     retweet: public_metrics.retweet_count,
                                     impression: public_metrics.impression_count,
                                     user: { connect: { id: user.id } },
+                                    postedAt: created_at ? new Date(created_at) : undefined,
                                 },
                             })
                         }
