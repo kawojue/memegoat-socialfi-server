580e61e9bbabf3197c0179e0f206eca707c82856
Raheem Kawojue <kawojue08@gmail.com>
2024-05-17 12:01:57 +0100

    perf: task

--- lib/task.service.ts
+++ lib/task.service.ts
@@ -16,7 +16,7 @@ export class TaskService {
         this.x = this.twit.readOnly
     }
 
-    @Cron(CronExpression.EVERY_10_MINUTES)
+    @Cron(CronExpression.EVERY_30_MINUTES)
     async metrics() {
         try {
             const settings = await this.prisma.settings.findFirst()
