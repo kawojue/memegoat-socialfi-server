8bf5d8c3b14a2d3601bf14c2fbc4c7005746cdcc
Raheem Kawojue <kawojue08@gmail.com>
2024-05-22 14:20:15 +0100

    perf: campaign

--- src/app.service.ts
+++ src/app.service.ts
@@ -186,12 +186,17 @@ export class AppService {
   async dashboard(res: Response, req: Request) {
     // @ts-ignore
     const profileId = req.user?.profileId
-    const { metadata, user, userRank, hasTurnedOffCampaign } = await this.info(
+    const info = await this.info(
       profileId,
       'profileId',
     )
     this.response.sendSuccess(res, StatusCodes.OK, {
-      data: { user, metadata, userRank, hasTurnedOffCampaign },
+      data: {
+        user: info.user,
+        metadata: info.metadata,
+        userRank: info.userRank,
+        hasTurnedOffCampaign: info.hasTurnedOffCampaign,
+      },
     })
   }
 
