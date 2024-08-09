784c90047e266c079f8763f58c233300f15ddfbc
Raheem Kawojue <kawojue08@gmail.com>
2024-08-09 18:41:30 +0100

    fix: redirect

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,7 +7,10 @@ import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common'
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  private isProd: boolean
+  constructor(private readonly authService: AuthService) {
+    this.isProd = process.env.NODE_ENV === "production"
+  }
 
   @Get('/x')
   @UseGuards(AuthGuard('twitter'))
@@ -22,18 +25,16 @@ export class AuthController {
     @Req() req: Request,
     @Res({ passthrough: true }) res: Response,
   ) {
-    const isProd = process.env.NODE_ENV === 'production'
-
     try {
       const token = await this.authService.auth(req)
 
       res.cookie('token', token, {
-        sameSite: isProd ? 'none' : 'lax',
-        secure: isProd,
+        sameSite: this.isProd ? 'none' : 'lax',
+        secure: this.isProd,
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
 
-      res.redirect('https://socialfi-memegoat.vercel.app/dashboard')
+      res.redirect('https://socialfi.memegoat.io/dashboard')
     } catch {
       res.redirect('https://socialfi.memegoat.io')
     }
@@ -41,11 +42,9 @@ export class AuthController {
 
   @Post('/logout')
   logout(@Res() res: Response) {
-    const isProd = process.env.NODE_ENV === 'production'
-
     res.clearCookie('token', {
-      sameSite: isProd ? 'none' : 'lax',
-      secure: isProd,
+      sameSite: this.isProd ? 'none' : 'lax',
+      secure: this.isProd,
     })
     res.sendStatus(204)
   }
